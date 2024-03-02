05ad88ceacf67cdd17f704fda99e5504eae0c893
wr <2119495210@qq.com>
2024-03-02 20:32:00 +0800

    2024/3/2

---
+++ js类型检测/js三类循环.js
@@ -0,0 +1,23 @@
+let arr = new Array(9999999).fill(0);
+
+// console.time('FOR LET~~');
+// for (let i = 0; i < arr.length; i++) {}
+// console.timeEnd('FOR LET~~');
+
+// console.time('WHILE~~');
+// let i = 0;
+// while (i < arr.length) {
+//     i++;
+// }
+// console.timeEnd('WHILE~~');
+
+console.time('FOR VAR~~');
+for (var i = 0; i < arr.length; i++) {}
+console.timeEnd('FOR VAR~~');
+
+console.time('WHILE VAR~~');
+var i = 0;
+while (i < arr.length) {
+    i++;
+}
+console.timeEnd('WHILE VAR~~');
---
+++ js类型检测/js类型检测.js
@@ -0,0 +1,8 @@
+let obj = {
+    name: '珠峰培训'
+};
+
+console.log("typeof: ", typeof obj);
+console.log("instanceof: ", obj instanceof Object);
+console.log("Object.prototype.toString.call: ", Object.prototype.toString.call(obj));
+console.log("Object.prototype.toString.call: ", Object.prototype.toString.call(3));
